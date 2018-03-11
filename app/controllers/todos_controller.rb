848da8ee55dbc6f164031b5761a588838c397bab
Askarbek Karasasev <askar.karasaev@gmail.com>
2018-03-11 12:26:32 +0900

    Section 2, Lecture 27 - Creating a new todo in UI completed

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,4 +4,22 @@ class TodosController < ApplicationController
     @todo = Todo.new
   end
 
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to todo_path(@todo)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:name, :description)
+  end
+
 end
